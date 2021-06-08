7c6af677225e0fa161d2422c4a48e16a06a3589b
Felicia Okta <feliciaokta@outlook.com>
2021-06-07 20:34:51 -0700

    finished DayListItem component

---
+++ src/components/DayListItem.js
@@ -0,0 +1,35 @@
+import React from "react";
+import classNames from 'classnames';
+import "components/DayListItem.scss";
+
+export default function DayListItem(props) {
+
+  const dayClass = classNames("day-list__item", {
+    "day-list__item--full": props.spots === 0,
+    "day-list__item--selected": props.selected,
+  })
+
+  const formatSpots = (spots) => {
+    let result = "";
+    if (spots === 0) {
+      result = "no spots remaining";
+    } else if (spots > 1) {
+      result = `${spots} spots remaining`;
+    } else if (spots === 1) {
+      result = `${spots} spot remaining`;
+    }
+    return result;
+  }
+
+  return (
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2> 
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
+    </li>
+  );
+}
+
+// unselected turns orange on hover
+// selected is white
+// full is grey
+// clickable turns orange on hover & sends message on click
