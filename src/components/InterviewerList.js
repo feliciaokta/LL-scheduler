3991d87c99e491afff577fb80e4fb62667529410
Felicia Okta <feliciaokta@outlook.com>
2021-06-08 16:33:37 -0700

    worked InterviewerList

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,3 +1,25 @@
 import React from "react";
 import classNames from 'classnames';
+import InterviewerListItem from "components/InterviewerListItem";
 import "components/InterviewerList.scss";
+
+export default function InterviewerList(props) {
+
+  const data = props.interviewers.map((interviewer, index) => {
+    return (<InterviewerListItem
+      key={index}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      selected={interviewer.id === props.interviewer}
+      setInterviewer={props.setInterviewer}
+    />)
+  })
+
+  return(
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">{data}</ul>
+    </section>
+  )
+
+};
