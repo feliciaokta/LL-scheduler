4c99fa4954166ab3d700274930eb167507dd9b1c
Felicia Okta <feliciaokta@outlook.com>
2021-06-08 17:04:50 -0700

    refactored InterviewerList

--- stories/index.js
+++ stories/index.js
@@ -112,7 +112,7 @@ storiesOf("InterviewerListItem", module)
       id={interviewer.id}
       name={interviewer.name}
       avatar={interviewer.avatar}
-      setInterviewer={action("setInterviewer")}
+      setInterviewer={event => action("setInterviewer")(interviewer.id)}
     />
   ));
 
