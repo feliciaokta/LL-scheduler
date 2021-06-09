da5fcbbb6be11b51b91c46d9912cb141c8e6519e
Felicia Okta <feliciaokta@outlook.com>
2021-06-08 17:43:58 -0700

    added appointments folder

---
+++ src/components/Appointments/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import classNames from 'classnames';
+import "components/Appointments/styles.scss";
+
+
+
+export default function Appointment(props) {
+
+  return (
+    <article className="appointment"></article>
+  )
+
+}
+
