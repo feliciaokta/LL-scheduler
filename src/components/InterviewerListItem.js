722fb922fe5fb51f17a9875cf45b73d122aeea05
Felicia Okta <feliciaokta@outlook.com>
2021-06-14 16:48:43 -0700

    passing previous tests and comments on props

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -3,6 +3,9 @@ import classNames from 'classnames';
 import "components/InterviewerListItem.scss";
 
 export default function InterviewerListItem(props) {
+
+  // console.log("props from InterviewerListItem: ", props);
+  // when you click the add appointment button, props are the 5 interviewers available for the day, each in a separate object
   
   // this const is for the scss
   const InterviewerClass = classNames("interviewers__item", {
